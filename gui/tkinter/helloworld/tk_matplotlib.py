7988494f41caa4e1fd219c5e46fc6eaa16d51262
masterj <ronchaos@gmail.com>
2019-06-24 17:50:25 +0900

    update tk_matplotlib
    add notebook select page func

--- gui/tkinter/helloworld/tk_matplotlib.py
+++ gui/tkinter/helloworld/tk_matplotlib.py
@@ -1,11 +1,10 @@
+import tkinter.ttk
 import tkinter as tk
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.animation as animation
-import numpy as np
 from time import sleep
-
 
 
 Data1 = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
@@ -15,37 +14,77 @@ Data1 = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
 df1 = DataFrame(Data1, columns=['Country', 'GDP_Per_Capita'])
 df1 = df1[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
 
-
-
 root = tk.Tk()
+
+notebook = tkinter.ttk.Notebook(root, width=600, height=500)
+notebook.pack()
+
+frame1 = tkinter.Frame(root)
+notebook.add(frame1, text="페이지1")
+
+frame2 = tkinter.Frame(root)
+notebook.add(frame2, text="페이지2")
+notebook_page_switch = 1
+
+
 '''
 ## fullscreen mode 
 root.attributes("-fullscreen", True)
 '''
+
 figure1 = plt.Figure(figsize=(6, 5), dpi=100)
 ax1 = figure1.add_subplot(111)
-bar1 = FigureCanvasTkAgg(figure1, root)
+
+bar1 = FigureCanvasTkAgg(figure1, frame1)
+#bar1 = FigureCanvasTkAgg(figure1, root)
 bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 df1.plot(kind='bar', legend=True, ax=ax1)
 ax1.set_title('Country Vs. GDP Per Capita')
 
+
 figure2 = plt.Figure(figsize=(5, 4), dpi=100)
 ax2 = figure2.add_subplot(111)
-canvas2 = FigureCanvasTkAgg(figure2, root)
+
+canvas2 = FigureCanvasTkAgg(figure2, frame2)
+#canvas2 = FigureCanvasTkAgg(figure2, root)
+
 canvas2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 ax2.set_title('Year Vs. Unemployment Rate')
 
+
+def move_cursor(event):
+    print("mouse ")
+    switchUI()
+
+
+def switchUI():
+    print("Switch UI Bt")
+    global notebook_page_switch
+    notebook.select(notebook_page_switch)
+    notebook_page_switch += 1
+    if notebook_page_switch > 1:
+        notebook_page_switch = 0
+
+
+
+figure1.canvas.mpl_connect("button_press_event", move_cursor)
+figure2.canvas.mpl_connect("button_press_event", move_cursor)
+#cid = plt.connect("motion_notify_event", move_cursor)
+
+button1 = tk.Button(root, overrelief="solid",text="test", width=15, command=switchUI, repeatdelay=1000, repeatinterval=100)
+button1.pack()
+
 def data_gen(t=0):
     cnt = 0
-    fb = open('/run/shm/vital_data.txt', 'r')
-    #fb = open('test.txt', 'r')
+    #fb = open('/run/shm/vital_data.txt', 'r')
+    fb = open('test.txt', 'r')
     while cnt < 100000:
         sleep(0.01)
         fb.seek(0)
         sbuf = fb.readline()
         split_str = sbuf.split(",", 1)
-        print(split_str[0])
-        print(split_str[1])
+        #print(split_str[0])
+        #print(split_str[1])
         cnt += 1
         t += 0.1
         yield t, int(split_str[0])
@@ -90,6 +129,5 @@ def run(data):
 
 ani = animation.FuncAnimation(figure2, run, data_gen, blit=False, interval=10,
                               repeat=False, init_func=init)
-#plt.show()
 
 root.mainloop()
